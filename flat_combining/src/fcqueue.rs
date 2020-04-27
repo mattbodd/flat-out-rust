a96f446ec4c6f502fa81c91573da885a35e558d9
Matt <mboddewy@u.rochester.edu>
2020-04-26 23:39:48 -0400

    Resolve all errors except for inability to store references accesssed by threads globally

--- flat_combining/src/fcqueue.rs
+++ flat_combining/src/fcqueue.rs
@@ -13,7 +13,6 @@ const COMBINING_NODE_TIMEOUT_CHECK_FREQUENCY: u64 = 100;
 const MAX_COMBINING_ROUNDS: u64 = 32;
 const NUM_ROUNDS_IS_LINKED_CHECK_FREQUENCY: u64 = 100;
 
-#[derive(Clone)]
 struct CombiningNode {
     is_linked: AtomicCell<bool>,
     last_request_timestamp: AtomicCell<u64>,
@@ -35,6 +34,7 @@ impl CombiningNode {
     }
 }
 
+#[derive(Clone)]
 struct QueueFatNode {
     items: Vec<i32>,
     items_left: usize,
@@ -46,7 +46,6 @@ impl QueueFatNode {
             // TOOD: Determine what size to initialize by default
             items: Vec::new(),
             items_left: 0,
-            next: None,
         }
     }
 }
@@ -57,6 +56,7 @@ struct FCQueue<'a> {
     current_timestamp: AtomicCell<u64>,
     comb_list_head: Mutex<VecDeque<&'a CombiningNode>>,
     queue: VecDeque<QueueFatNode>,
+    trash: Arc<Mutex<VecDeque<CombiningNode>>>,
 }
 
 impl<'a> FCQueue<'a> {
@@ -67,11 +67,12 @@ impl<'a> FCQueue<'a> {
             current_timestamp: AtomicCell::new(0),
             comb_list_head: Mutex::new(VecDeque::new()),
             queue: VecDeque::new(),
+            trash: Arc::new(Mutex::new(VecDeque::new())),
         }
     }
-	
+
     fn doFlatCombining(&mut self) {
-    	let mut combining_round: u64 = 0;
+        let mut combining_round: u64 = 0;
         let mut num_pushed_items: usize = 0;
         let mut curr_comb_node: VecDeque<&CombiningNode>;
         {
@@ -80,7 +81,7 @@ impl<'a> FCQueue<'a> {
         let mut last_combining_node: Option<usize> = None;
         self.current_timestamp.fetch_add(1);
         let local_current_timestamp: u64 = self.current_timestamp.load();
-        let mut local_queue_head: VecDeque<QueueFatNode> = self.queue;
+        let mut local_queue_head: VecDeque<QueueFatNode> = self.queue.clone();
 
         let check_timestamps: bool =
             local_current_timestamp % COMBINING_NODE_TIMEOUT_CHECK_FREQUENCY == 0;
@@ -179,11 +180,11 @@ impl<'a> FCQueue<'a> {
                 let mut new_node: QueueFatNode = QueueFatNode::new();
                 new_node.items_left = num_pushed_items;
                 new_node.items = Vec::with_capacity(num_pushed_items);
-                for an_item in self.combined_pushed_items {
-                    new_node.items.push(an_item)
+                for an_item in &self.combined_pushed_items {
+                    new_node.items.push(*an_item)
                 }
 
-                self.queue.append(new_node);
+                self.queue.push_back(new_node);
             }
 
             combining_round += 1;
@@ -194,10 +195,8 @@ impl<'a> FCQueue<'a> {
             }
         }
     }
-       
-    
 
-    fn link_in_combining(&self, cn: &CombiningNode) {
+    fn link_in_combining(&self, cn: &'a CombiningNode) {
         // Block until we have access to the global `comb_list_head` at which point
         // we merge our thread local queue
         let mut curr_comb_queue = self.comb_list_head.lock().unwrap();
@@ -205,13 +204,13 @@ impl<'a> FCQueue<'a> {
         //  Mutex is unlocked at end of scope
     }
 
-    fn wait_until_fulfilled(&self, comb_node: CombiningNode) {
+    fn wait_until_fulfilled(&mut self, comb_node: CombiningNode) {
         let mut rounds = 0;
 
         loop {
             if (rounds % NUM_ROUNDS_IS_LINKED_CHECK_FREQUENCY == 0) && !comb_node.is_linked.load() {
                 comb_node.is_linked.store(true);
-                self.link_in_combining(comb_node);
+                self.link_in_combining(&comb_node);
             }
 
             if self.fc_lock.load(Ordering::Relaxed) == 0 {
@@ -224,40 +223,20 @@ impl<'a> FCQueue<'a> {
                 }
 
                 if !comb_node.is_request_valid.load() {
-                    return;
+                    break;
                 }
 
                 rounds += 1;
             }
         }
+
+        let clone = Arc::clone(&self.trash);
+        let mut _trash = clone.lock().unwrap();
+        _trash.push_back(comb_node);
     }
 
-    fn enqueue(&self, val: i32) -> bool {
-        // Combining node should be a thread local variable
-        /*
-        let mut comb_node: Option<CombiningNode> = None;
-        FCQueue::combining_node.with(|cn| {
-            // Create new instance of combining node using old instance's values
-            comb_node = Some(CombiningNode {
-                is_linked: AtomicCell::new(cn.is_linked.take()),
-                last_request_timestamp: AtomicCell::new(cn.last_request_timestamp.take()),
-                is_request_valid: AtomicCell::new(cn.is_request_valid.take()),
-                is_consumer: AtomicCell::new(cn.is_consumer.take()),
-                item: AtomicCell::new(cn.item.take()),
-            });
-        });
-         */
-
-        thread_local! {
-            static combining_node: CombiningNode = CombiningNode::new();
-        }
-
-        /*
-        let comb_node: CombiningNode = match comb_node {
-            Some(cn) => cn,
-            None => panic!("No combining node found in `enqueue`"),
-        };
-         */
+    fn enqueue(&mut self, val: i32) -> bool {
+        let combining_node: CombiningNode = CombiningNode::new();
 
         combining_node.is_consumer.store(false);
         combining_node.item.store(Some(val));
