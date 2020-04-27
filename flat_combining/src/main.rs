5b21786a9eff19c5d4b02782ce9eb44081c5fc0b
Matt <mboddewy@u.rochester.edu>
2020-04-27 13:59:21 -0400

    Additional debugging with multiple entries

--- flat_combining/src/main.rs
+++ flat_combining/src/main.rs
@@ -2,15 +2,17 @@ mod datastructures;
 use datastructures::{queue, stack};
 
 mod fcqueue;
-use fcqueue::{FCQueue};
+use fcqueue::FCQueue;
 
 fn main() {
-
-
-    let mut new_queue:FCQueue =FCQueue::new();
+    let mut new_queue: FCQueue = FCQueue::new();
 
     new_queue.enqueue(3);
+    new_queue.enqueue(4);
+    new_queue.enqueue(5);
     new_queue.queue[0].get();
-
-
+    println!("---");
+    new_queue.queue[1].get();
+    println!("---");
+    new_queue.queue[2].get();
 }
