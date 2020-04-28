85ec9f52afcb1875c9bdcf0229ad906ba761f310
Matt <mboddewy@u.rochester.edu>
2020-04-28 19:05:33 -0400

    Demonstrate concurrent execution

--- flat_combining/src/main.rs
+++ flat_combining/src/main.rs
@@ -1,12 +1,34 @@
 mod fcqueue;
+use crossbeam_utils::thread;
 use fcqueue::FCQueue;
-//use std::thread;
-use crossbeam_utils::thread::scope;
-
-
-
+use std::sync::Arc;
 
 fn main() {
+    let queue = FCQueue::new();
+
+    let ten_millis = std::time::Duration::from_millis(1000);
+
+    thread::scope(|s| {
+        let shared_queue = Arc::new(&queue);
+        for i in 1..4 {
+            let cloned_shared_queue = Arc::clone(&shared_queue);
+            s.spawn(move |_| {
+                if i == 1 {
+                    std::thread::sleep(ten_millis);
+                    std::thread::sleep(ten_millis);
+                    std::thread::sleep(ten_millis);
+                    cloned_shared_queue.enqueue(i);
+                } else {
+                    cloned_shared_queue.enqueue(i);
+                }
+                if i == 2 {
+                    std::thread::sleep(ten_millis);
+                    println!("dequeued: {}", cloned_shared_queue.dequeue());
+                }
+            });
+        }
+    })
+    .unwrap();
 
     // new_queue.enqueue(3);
     // new_queue.enqueue(4);
@@ -23,22 +45,4 @@ fn main() {
     // println!("{}", new_queue.dequeue());
     // println!("{}", new_queue.dequeue());
     // println!("{}", new_queue.dequeue());
-
-    println!("---");
-
-
-
-	const COUNT: usize  = 50;
-    const THREADS: usize = 4;
-
-    let q :FCQueue = FCQueue::new();
-    q.enqueue(3);
-    println!("{}", q.dequeue());
-		
-
-    
-	
-
-	//println!("{}", new_queue.dequeue());
-	
 }
