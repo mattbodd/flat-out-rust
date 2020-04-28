b2d451c7ac83255b0d1179582824fc3fd2d3f1a2
Matt <mboddewy@u.rochester.edu>
2020-04-28 17:19:10 -0400

    Resolve merge conflicts

--- flat_combining/src/main.rs
+++ flat_combining/src/main.rs
@@ -7,6 +7,9 @@ fn main() {
     new_queue.enqueue(3);
     new_queue.enqueue(4);
     new_queue.enqueue(5);
+
+    println!("---");
+    new_queue.queue[0].get();
     println!("---");
     new_queue.queue[1].get();
     println!("---");
