5e3337562a89c89ac409443f5d92c29e34391968
Matthew Boddewyn <mboddewy@node2x18a.csug.rochester.edu>
2020-04-28 22:00:46 -0400

    Run parallel stress test

--- flat_combining/src/lib.rs
+++ flat_combining/src/lib.rs
@@ -1,16 +1,28 @@
+use crossbeam_utils::thread;
+use std::sync::Arc;
 mod fcqueue;
 use fcqueue::FCQueue;
 
 static SOME_ELEMS: i32 = 10;
-static MANY_ELEMS: i32 = 1_000_000;
+static MANY_ELEMS: i32 = 100_000;
+static NUM_THREADS: i32 = 4;
+static ELEMS_PER_THREAD: i32 = MANY_ELEMS / NUM_THREADS;
 
 pub fn fc_test() {
     let queue = FCQueue::new();
 
-    // Enqueue `num_elems` elements
-    for elem in 0..MANY_ELEMS {
-        queue.enqueue(elem);
-    }
+    thread::scope(|s| {
+        let shared_queue = Arc::new(&queue);
+        for i in 0..NUM_THREADS {
+            let cloned_shared_queue = Arc::clone(&shared_queue);
+            s.spawn(move |_| {
+                for elem in 0..ELEMS_PER_THREAD {
+                    cloned_shared_queue.enqueue(elem);
+                }
+            });
+        }
+    })
+    .unwrap();
 }
 
 pub mod seq {
@@ -135,13 +147,7 @@ pub mod seq {
 }
 
 mod par {
-    use crossbeam_utils::thread;
-    use std::sync::Arc;
-
     use super::*;
-
-    static NUM_THREADS: i32 = 4;
-    static ELEMS_PER_THREAD: i32 = MANY_ELEMS / NUM_THREADS;
 
     #[test]
     fn stress_enqueue() {
