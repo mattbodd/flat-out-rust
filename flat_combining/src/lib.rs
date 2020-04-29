535dc0d4de2d7c36f56962c1e0e66f7c3d87cef3
Matt <mboddewy@u.rochester.edu>
2020-04-28 20:24:32 -0400

    Update style

--- flat_combining/src/lib.rs
+++ flat_combining/src/lib.rs
@@ -1,16 +1,11 @@
-pub mod seq_list;
 mod fcqueue;
+pub mod seq_list;
 use crossbeam_utils::thread;
 use fcqueue::FCQueue;
 use std::sync::Arc;
 
-
-
-
-
-
-pub fn fc_test(){
-	let queue = FCQueue::new();
+pub fn fc_test() {
+    let queue = FCQueue::new();
 
     let ten_millis = std::time::Duration::from_millis(100);
 
@@ -19,48 +14,37 @@ pub fn fc_test(){
         for i in 0..4 {
             let cloned_shared_queue = Arc::clone(&shared_queue);
             s.spawn(move |_| {
-            	cloned_shared_queue.enqueue(i);
-                
-                    //println!("dequeued: {}", cloned_shared_queue.dequeue());
-                
+                cloned_shared_queue.enqueue(i);
+
+                //println!("dequeued: {}", cloned_shared_queue.dequeue());
             });
         }
     })
     .unwrap();
-
 }
 
+mod tests {
+    use super::*;
 
+    #[test]
+    fn enqueue() {
+        let queue = FCQueue::new();
+        queue.enqueue(1);
+        queue.enqueue(2);
+        queue.enqueue(3);
+        queue.enqueue(4);
+    }
 
-mod tests{
-	use super::*;
-
-	#[test]
-	fn enqueue(){
-		let queue = FCQueue::new();
-		queue.enqueue(1);
-		queue.enqueue(2);
-		queue.enqueue(3);
-		queue.enqueue(4);
-
-
-	}
-
-	#[test]
-	fn dequeue(){
-		let queue = FCQueue::new();
-		queue.enqueue(1);
-		queue.enqueue(2);
-		queue.enqueue(3);
-		queue.enqueue(4);
-		println!("dequeued: {}", queue.dequeue());
-		println!("dequeued: {}", queue.dequeue());
-		println!("dequeued: {}", queue.dequeue());
-		println!("dequeued: {}", queue.dequeue());
-		
-	}
-
-
-
-
+    #[test]
+    fn dequeue() {
+        let queue = FCQueue::new();
+        queue.enqueue(1);
+        queue.enqueue(2);
+        queue.enqueue(3);
+        queue.enqueue(4);
+        println!("dequeued: {}", queue.dequeue());
+        println!("dequeued: {}", queue.dequeue());
+        println!("dequeued: {}", queue.dequeue());
+        println!("dequeued: {}", queue.dequeue());
+    }
 }
