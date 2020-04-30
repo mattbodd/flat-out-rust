bc5880ae5dfded9e189b006e71c2379059e06896
Matthew Boddewyn <mboddewy@node2x18a.csug.rochester.edu>
2020-04-30 18:11:57 -0400

    Update parse file

--- flat_combining/profiling/parse.py
+++ flat_combining/profiling/parse.py
@@ -1,3 +1,13 @@
+import sys
+from os import walk
+
+files = []
+for (dirpath, dirnames, filenames) in walk(sys.argv[1]):
+    files.extend(filenames)
+    break
+
+print("File: " + str(files))
+
 runtimes = {}
 
 def parse_line(line):
@@ -16,19 +26,32 @@ def parse_line(line):
         pass
 
 
-file = open(input("Enter file path: "))
-for line_num, line in enumerate(file):
-    if line_num == 0:
-        continue
-    if line_num == 1:
-        continue
-    parse_line(line)
+for _file in files:
+    if sys.argv[2] in _file:
+        print("---", _file, "---")
+        for line_num, line in enumerate(open(sys.argv[1] + '/' + _file)):
+            if line_num == 0:
+                continue
+            if line_num == 1:
+                continue
+            parse_line(line)
 
-for func in runtimes:
-    for _id in runtimes[func]:
-        total = 0
-        avg = 0
-        for runtime in runtimes[func][_id]:
-            total += 1
-            avg += int(runtime)
-        print("{" + _id + "} " + func + ": " + str(avg/total) + "ns | " + str(total) + " times")
+        for func in runtimes:
+            all_total = 0
+            all_thread_avg = 0
+            max_thread_time = 0
+            for _id in runtimes[func]:
+                total = 0
+                avg = 0
+                for runtime in runtimes[func][_id]:
+                    total += 1
+                    avg += int(runtime)
+                    max_thread_time = max(max_thread_time, int(runtime))
+
+                all_total += total
+                all_thread_avg += avg
+
+                # print("{" + _id + "} " + func + ": " + str(avg/total) + "ns | " + str(total) + " times")
+            print(func + " avg: " + str(all_thread_avg/all_total) + "ns | " + str(all_thread_avg) + " times")
+            print(func + " max: " + str(max_thread_time) + "ns | " + str(all_thread_avg) + " times")
+        runtimes = {}
